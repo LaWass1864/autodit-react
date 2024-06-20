e04bcef2ee9878cf53aafa9a2c21468ecb338983
lawass1864 <amwass4@hotmail.com>
2024-06-21 00:22:29 +0200

    header ok

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,8 +3,11 @@ import React from 'react';
 const Header = () => {
   // Variables pour afficher les informations
   const name = "Wassila";
-  const job = "Développeuse web front-end";
+  const job = "Développeuse\nfront-end";
   const availability = "Disponible";
+
+  const title = "Portfolio";
+  const subtitle = "Controle de connaissance";
 
   // Fonction pour formater la date
   const formatDate = (date) => {
@@ -17,19 +20,19 @@ const Header = () => {
   return (
     <header className="flex justify-between items-start p-4 rounded-t-xl w-full max-w-4xl">
       <div className="flex flex-col items-start space-y-1">
-        <h1 className="text-xl font-gloria text-custom-blue underline-red">{name}</h1>
-        <p className="text-sm max-w-[90px] font-gloria text-custom-blue underline-red">{job}</p> 
+        <h1 className="text-lg font-gloria text-custom-blue underline-red">{name}</h1>
+        <p className="text-base font-gloria text-custom-blue underline-red whitespace-pre-wrap">{job}</p>
         <div className="flex items-center space-x-2">
-          <div className="bg-green-500 w-3 h-3 rounded-full"></div>
-          <p className="text-sm text-green-500 font-gloria">{availability}</p>
+        <div className= "w-3 h-3 rounded-full " style={{ backgroundColor: 'var(--screamin-green)' }}></div>
+          <p className="text-lg font-gloria text-custom-blue" >{availability}</p>
         </div>
       </div>
       <div className="text-center self-start">
-        <h2 className="text-2xl font-gloria text-custom-blue underline-red">Portfolio</h2>
-        <p className="text-sm font-gloria text-custom-blue">Contrôle de connaissance</p>
+        <h2 className="text-2xl font-gloria text-custom-blue underline-red">{title}</h2>
+        <p className="text-lg font-gloria text-custom-blue">{subtitle}</p>
       </div>
       <div className="text-right self-start">
-        <p className="text-sm font-gloria text-custom-blue underline-red">{today}</p>
+        <p className="text-lg font-gloria text-custom-blue underline-red">{today}</p>
       </div>
     </header>
   );
