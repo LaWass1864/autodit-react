a9289846682811b03bd61a9205ab8b535920a31a
lawass1864 <amwass4@hotmail.com>
2024-06-20 18:55:18 +0200

    header ok avec le surlignement dans le fichier index.css

--- src/component/Header.js
+++ src/component/Header.js
@@ -17,19 +17,19 @@ const Header = () => {
   return (
     <header className="flex justify-between items-start p-4 rounded-t-xl w-full max-w-4xl">
       <div className="flex flex-col items-start space-y-1">
-        <h1 className="text-xl font-bold font-gloria">{name}</h1>
-        <p className="text-sm max-w-[90px] font-gloria">{job}</p> 
+        <h1 className="text-xl font-gloria text-custom-blue underline-red">{name}</h1>
+        <p className="text-sm max-w-[90px] font-gloria text-custom-blue underline-red">{job}</p> 
         <div className="flex items-center space-x-2">
           <div className="bg-green-500 w-3 h-3 rounded-full"></div>
           <p className="text-sm text-green-500 font-gloria">{availability}</p>
         </div>
       </div>
       <div className="text-center self-start">
-        <h2 className="text-2xl font-bold font-gloria">Portfolio</h2>
-        <p className="text-sm font-gloria">Contrôle de connaissance</p>
+        <h2 className="text-2xl font-gloria text-custom-blue underline-red">Portfolio</h2>
+        <p className="text-sm font-gloria text-custom-blue">Contrôle de connaissance</p>
       </div>
       <div className="text-right self-start">
-        <p className="text-sm font-gloria">{today}</p>
+        <p className="text-sm font-gloria text-custom-blue underline-red">{today}</p>
       </div>
     </header>
   );
