f66aa596aeb0ce7a09719f61f505206556905bba
lawass1864 <amwass4@hotmail.com>
2024-06-21 16:52:30 +0200

    encadré avec formulaire ok. Les infos sont envoyées vers boite gmail

--- src/component/Note.js
+++ src/component/Note.js
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Note = () => {
+  const form = useRef();
 
-    const note = "/20";
-    const appreciation = "Très bon travail, continuez ainsi!";
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    return (
-        <div className="note-appreciation-container">
-        <div className="note-title-container">
-          <h3 className="note-title">Note: {note}</h3>
-        </div>
-        <div className="vertical-line"></div>
-        <div className="note-text-container">
-          <p className="note-text">{appreciation}</p>
+    emailjs.sendForm(
+      'service_57t1xy2', // Remplacez par votre Service ID
+      'template_rspn9qe', // Remplacez par votre Template ID
+      form.current,
+      'oCpRBtFl9XwOD5fyi'  // Remplacez par votre Clé publique
+    )
+    .then((result) => {
+      console.log(result.text);
+      alert('Votre note et appréciation ont été envoyées, merci ;)!');
+    }, (error) => {
+      console.log(error.text);
+      alert('Une erreur s\'est produite, veuillez réessayer.');
+    });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail} className="note-appreciation-container">
+      <div className="note-title-container">
+        <div className="note-content">
+          <label htmlFor="note" className="note-label"></label>
+          <input type="number" id="note" name="note" className="note-value font-gloria bg-transparent" required min="0" max="20" />
+          <span className="max-note font-gloria">/20</span>
         </div>
       </div>
-    );
+      <div className="note-text-container">
+        <label htmlFor="appreciation" className="appreciation-label"> </label>
+        <textarea id="appreciation" name="appreciation" className="note-text font-gloria bg-transparent" required></textarea>
+      </div>
+      <div className="user-info-container">
+        <label htmlFor="user_name">Nom:</label>
+        <input type="text" id="user_name" name="user_name" className="user-name-input font-gloria bg-transparent" required />
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" className="email-input font-gloria bg-transparent" required />
+      </div>
+      <button type="submit" className="submit-button rounded-lg font-merriweather px-4" style={{ 
+        backgroundColor: 'var(--screamin-green)'
+      }}>Notez</button>
+    </form>
+  );
 };
 
 export default Note;
