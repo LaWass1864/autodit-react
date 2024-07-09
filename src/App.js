2ba7795459264fc9ef3b6717501a7b6c983af0da
lawass1864 <amwass4@hotmail.com>
2024-07-09 15:11:58 +0200

    stylisation des fleche du carrousel

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import Paper from './component/Paper'; // Assurez-vous que le chemin est correct
+import Paper from './component/Paper'; 
 import PresentationPage from './component/PresentationPage'; // Assurez-vous que le chemin est correct
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+
 
 const App = () => {
   const [showPresentation, setShowPresentation] = useState(true);
@@ -18,6 +19,7 @@ const App = () => {
       ) : (
         <div className="min-h-screen flex items-center justify-center mt-12">
           <Paper />
+ 
         </div>
       )}
     </div>
