890fbee6b33959272819222d3076afe70d34653b
lawass1864 <amwass4@hotmail.com>
2024-07-09 16:52:52 +0200

    bracket pour le projet-lien ok

--- src/component/Projets.js
+++ src/component/Projets.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import projets from '../data/projects.json';
-
+import { annotate } from 'rough-notation';
 
 const Projets = () => {
+  useEffect(() => {
+    const elements = document.querySelectorAll('.block');
+    elements.forEach(element => {
+      const annotation = annotate(element, { type: 'bracket', color: 'red', padding: [2, 10], brackets: ['left', 'right'], strokeWidth: 3 });
+      annotation.show();
+    });
+  }, []); 
+
   return (
     <div className="projets">
       {projets.map((projet, index) => (
@@ -11,8 +19,8 @@ const Projets = () => {
           <div className="projet-info">
             <h3 className="projet-nom font-gloria mb-2">{projet.nom}</h3>
             <p className="projet-description font-gloria text-custom-blue" dangerouslySetInnerHTML={{ __html: projet.description }}></p>
-            <p className="projet-lien font-gloria text-custom-blue font-semibold">
-              <a href={projet.github} >GitHub</a> | <a href={projet.site_web} >Site Web</a>
+            <p className="block projet-lien font-gloria text-custom-blue font-semibold">
+              <a href={projet.github}>GitHub</a> | <a href={projet.site_web}>Site Web</a>
             </p>
           </div>
         </div>
